92c68db10473b2f4e04f51b7c035783c780415b1
Martin Hasmat <mhasmat@gmail.com>
2024-07-24 20:21:28 -0300

    upload tarea

---
+++ src/components/CartaPresentacion.jsx
@@ -0,0 +1,21 @@
+const CartaPresentacion = () => {
+    return (
+        <>
+        <p> 
+            <h1>Soy Martín Hasmat</h1>
+            <li>Estudiante de programación web in progress...</li>
+            <p>Mi recorrido incluye:</p>                
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>Javascript</li>
+            <li>Python</li>
+            <li>Django</li>            
+            <p>{'>>>'} Actualmente cursando {'<<<'}</p>
+            <h4><li>React JS</li></h4>            
+            <h3>Quiero convertirme en Full-Stack Developer!</h3>
+        </p>
+        </>
+    )
+}
+
+export default CartaPresentacion;
